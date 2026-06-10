333f923b3cffe995dacf8e7cc72c52486e90b9d7
Enkhmend <enkhmendjargal3838@gmail.com>
2026-06-10 18:12:37 +0800

    backend anhnii

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import pool from "./config/db.js";
 dotenv.config();
 const app=express();
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
 app.get("/", (req, res)=>{
     res.send("ajillaj baina");
 });
-const port=process.env.port || 5000;
+app.get("/api/test", async (req, res)=>{
+    try{
+        const a=await pool.query("select * from users");
+        res.json({message: "db holbogdson", data: a.rows,});
+    }
+    catch(error){
+        res.status(500).json({message: "db holbolt amjiltgui", error: error.message});
+    }
+});
+const port=process.env.PORT || 5000;
 app.listen(port, ()=>{
     console.log(`server: ${port}`);
 })
