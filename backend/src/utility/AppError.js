e30243ca6aa6e5346a95549777a937fe5043d25b
Otgoo <Otgontsetsegg456@.com>
2026-06-19 17:21:59 +0800

    Хуучин client устгав

--- backend/src/utility/AppError.js
+++
@@ -1,9 +0,0 @@
-export default class AppError extends Error {
-  constructor(message, statusCode) {
-    super(message);
-    this.statusCode = statusCode;
-    this.status = String(statusCode).startsWith("4")? "fail": "error";
-    this.isOperational = true;
-    Error.captureStackTrace(this, this.constructor);
-  }
-}
