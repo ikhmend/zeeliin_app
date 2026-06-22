4b73f315341ed55956cbe520dabcf9e93db96a01
Otgoo <Otgontsetsegg456@.com>
2026-06-22 17:50:29 +0800

    url

--- loan-system/src/api/api.js
+++ loan-system/src/api/api.js
@@ -5,7 +5,6 @@ const api = axios.create({
     // cookie accept
     withCredentials: true, 
 });
-
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
     if (token) {
