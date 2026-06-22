963fd80accff62e9719eff2aa5d7c4ba09a044d7
Otgoo <Otgontsetsegg456@.com>
2026-06-22 17:45:10 +0800

    Update url

--- loan-system/src/api/api.js
+++ loan-system/src/api/api.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-    // Бэкэнд ажиллаж буй IP хаяг
-    baseURL: "http://10.0.100.172:5000/api", 
-    // Күүкигээр мэдээлэл солилцохыг зөвшөөрөх (ERR_FAILED алдааг засна)
+    baseURL: "http://143.198.215.216:5000/api", 
+    // cookie accept
     withCredentials: true, 
 });
 
-// Хүсэлт илгээх бүрд токен шалгаж Header-т нэмэх
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
     if (token) {
